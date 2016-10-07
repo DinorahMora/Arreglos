ec00a941c981c0332ba42954d0f6d490da2f0d44
DinorahMora <cintyamorar@live.com.mx>
2016-10-07 15:05:21 -0500

    arreglo de 5

---
+++ arreglo.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+int arreglo[5];
+
+arreglo[0]= 1;
+arreglo[1]= 2;
+arreglo[2]= 3;
+arreglo[3]= 4;
+arreglo[4]= 5;
+
+printf("Arreglo: {%i,%i,%i,%i,%i}",arreglo[0],arreglo[1],arreglo[2],arreglo[2],arreglo[4]);
+
+  return 0;
+}
