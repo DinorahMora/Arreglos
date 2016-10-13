80f1008b0d73b2d20446d91e5f45e1625b3295c0
DinorahMora <cintyamorar@live.com.mx>
2016-10-13 18:55:06 -0500

    arreglo de 15 numeros

--- arreglo.cpp
+++ arreglo.cpp
@@ -2,10 +2,10 @@
 
 int main() {
 
-int arreglo[10]={1,2,3,4,5,6,7,8,9,10};
+int arreglo[15]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
 
-for(int i=0; i <10; i++) {
+for(int i=0; i <15; i++) {
 printf("%i ",arreglo[i]);
 }
 
